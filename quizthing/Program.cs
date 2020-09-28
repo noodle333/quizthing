6f44da2f863c0a826d1a3cc078f89a02598d23e0
Oliver <oliver.nordin@elev.ga.ntig.se>
2020-09-28 08:36:16 +0200

    added while loops

--- quizthing/Program.cs
+++ quizthing/Program.cs
@@ -11,42 +11,73 @@ namespace quizthing
             string p_answer2;
             string p_answer3;
 
+            int a = 1;
+            int b = 1;
+            int c = 1;
+
             Console.WriteLine("THIS IS A 3 QUESTION QUIZ. A QUESTION WILL BE ASKED AND YOU WILL CHOOSE ONE OF THE FOLLOWING ALTERNATIVES.");
-            Console.WriteLine("WHAT IS THE RESULT OF √144 + √169?");
-            Console.WriteLine("A: 25 | B: 28 | C: 16");
-            p_answer1 = Console.ReadLine().ToLower();
-            if (p_answer1 == "a")
+            while (a == 1)
             {
-                Console.WriteLine("CORRECT.");
-                points = points + 1;
+                Console.WriteLine("WHAT IS THE RESULT OF √144 + √169?");
+                Console.WriteLine("A: 25 | B: 28 | C: 16");
+                p_answer1 = Console.ReadLine().ToLower();
+                if (p_answer1 == "a")
+                {
+                    Console.WriteLine("CORRECT.");
+                    points = points + 1;
+                    a = a - 1;
+                }
+                else if (p_answer1 == "b" || p_answer1 == "c")
+                {
+                    Console.WriteLine("INCORRECT.");
+                    a = a - 1;
+                }
+                else
+                {
+                    Console.WriteLine("INCORRECT INPUT TRY AGAIN.");
+                }
             }
-            else
+            while (b == 1)
             {
-                Console.WriteLine("INCORRECT.");
+                Console.WriteLine("WHICH COUNTRY HAS THE LARGEST POPULATION?");
+                Console.WriteLine("A: JAPAN | B: RUSSIA | C: BRAZIL");
+                p_answer2 = Console.ReadLine().ToLower();
+                if (p_answer2 == "c")
+                {
+                    Console.WriteLine("CORRECT.");
+                    points = points + 1;
+                    b = b - 1;
+                }
+                else if (p_answer2 == "a" || p_answer2 == "b")
+                {
+                    Console.WriteLine("INCORRECT");
+                    b = b - 1;
+                }
+                else
+                {
+                    Console.WriteLine("INCORRECT INPUT TRY AGAIN.");
+                }
             }
-            Console.WriteLine("WHICH COUNTRY HAS THE LARGEST POPULATION?");
-            Console.WriteLine("A: JAPAN | B: RUSSIA | C: BRAZIL");
-            p_answer2 = Console.ReadLine().ToLower();
-            if (p_answer2 == "c")
+            while (c == 1)
             {
-                Console.WriteLine("CORRECT.");
-                points = points + 1;
-            }
-            else
-            {
-                Console.WriteLine("INCORRECT");
-            }
-            Console.WriteLine("WHO IS THE MAIN CREATOR OF THE GAME UNDERTALE?");
-            Console.WriteLine("A: MICHEAL JACKSON | B: BRADLEY COOPER | C: TOBY FOX");
-            p_answer3 = Console.ReadLine().ToLower();
-            if (p_answer3 == "c")
-            {
-                Console.WriteLine("CORRECT.");
-                points = points + 1;
-            }
-            else
-            {
-                Console.WriteLine("INCORRECT.");
+                Console.WriteLine("WHO IS THE MAIN CREATOR OF THE GAME UNDERTALE?");
+                Console.WriteLine("A: MICHEAL JACKSON | B: BRADLEY COOPER | C: TOBY FOX");
+                p_answer3 = Console.ReadLine().ToLower();
+                if (p_answer3 == "c")
+                {
+                    Console.WriteLine("CORRECT.");
+                    points = points + 1;
+                    c = c - 1;
+                }
+                else if (p_answer3 == "a" || p_answer3 == "b")
+                {
+                    Console.WriteLine("INCORRECT.");
+                    c--;
+                }
+                else
+                {
+                    Console.WriteLine("INCORRECT INPUT TRY AGAIN.");
+                }
             }
             if (points == 0 || points == 1)
             {
